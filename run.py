009378aba1a62fdc92b3ccdf9bfff1d4b41e7f8b
Emmanuel Amobi <amobisomto@gmail.com>
2022-04-03 16:40:30 -0500

    logging clicks

--- run.py
+++ run.py
@@ -3,13 +3,6 @@ from board import Board
 from ui_consts import W, H
 from utils import get_index_from_click
 
-# UI variables 
-w,h = 720, 720
-each_square = w / 8 # divide width by number of rows or cols to get square size
-red = (255, 0, 0)
-white = (255, 255, 255)
-black = (0, 0, 0)
-
 # initialize window
 WIN = pygame.display.set_mode((W,H))
 pygame.display.set_caption('checkers minmax')
@@ -32,7 +25,6 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 click = pygame.mouse.get_pos()
                 row, col = get_index_from_click(click)
-                print("ROW COL: ", row, col)
                 piece = board.get_piece(row,col)
                 print(piece)
                 pass
